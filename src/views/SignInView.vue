6f4500b2180fb847d69c921a73c7f696733e6df2
banshay <importantbopp@gmail.com>
2022-10-23 00:01:50 +0200

    Update client ids

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -2,7 +2,7 @@
   <div class="h-screen flex items-center justify-center">
     <div
       id="g_id_onload"
-      data-client_id="82634285895-4p3or8l8g9quvlo7ueu9r13sfc7esa23.apps.googleusercontent.com"
+      data-client_id="405975567512-ml31fikaagpbtni851nplbhpfg2tkqop.apps.googleusercontent.com"
       data-callback="loginCallback"
       data-auto_prompt="false"
     ></div>
